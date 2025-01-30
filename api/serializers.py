b9c585fc8ee961a353923d87ebd852e45005cc0d
P1R47E <yosephtesfaye27@gmail.com>
2025-01-30 09:41:33 +0300

    serializer for order model

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,20 @@
 from rest_framework import serializers
-from .models import Product, Order, Cart, CartItem
+from .models import Product, Order, Cart, CartItem,OrderItem
 
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = '__all__'
+
+class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Order
+        fields = '__all__'
+        
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
